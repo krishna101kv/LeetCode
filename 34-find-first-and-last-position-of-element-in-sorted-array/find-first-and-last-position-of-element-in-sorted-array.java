7f0ce7ed0357cc479acdebdeaa014503598ea4c7
Krishna <84918659+krishna101kv@users.noreply.github.com>
2024-03-16 08:02:15 -0700

    Time: 0 ms (100.00%) | Memory: 45.8 MB (49.14%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int[] a={-1,-1};
-        for(int i=0;i<nums.length;i++){
-                if(nums[i]==target){
-                        a[0]=i;
-                        break;
-                }
-        }
-        for(int i=nums.length-1;i>=0;i--){    
-            if(nums[i]==target){ 
-                a[1]=i;
-                break;
+        int left=binSearch(nums,target,true);
+        int right=binSearch(nums,target,false);
+        int[] arr=new int[2];
+        arr[0]=left;
+        arr[1]=right;
+        return arr;
+    }
+    public static int binSearch(int[] nums, int target, boolean isleft){
+        int s=0;
+        int e=nums.length-1;
+        int i=-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(nums[m]>target){
+                e=m-1;
             }
-                
-               
+            else if(nums[m]< target){
+                s=m+1;
+            }
+            else{
+                i=m;
+                if(isleft){
+                    e=m-1;
+                }
+                else{
+                    s=m+1;
+                }
+            }
         }
-        return a;
-        
+        return i;
     }
 }
+ //  int[] a={-1,-1};
+        // for(int i=0;i<nums.length;i++){
+        //         if(nums[i]==target){
+        //                 a[0]=i;
+        //                 break;
+        //         }
+        // }
+        // for(int i=nums.length-1;i>=0;i--){    
+        //     if(nums[i]==target){ 
+        //         a[1]=i;
+        //         break;
+        //     }
+               
+        // }
+        // return a;
