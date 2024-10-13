7bc280e1891650be1f00f4a7ac05637b0511ea96
Krishna <84918659+krishna101kv@users.noreply.github.com>
2024-10-12 21:26:03 -0700

    Time: 2 ms (98.80%) | Memory: 43 MB (94.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,16 +1,36 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int ans=0;
-        int rel=0;
-        for(int i=0;i<s.length();i++) {
-            Character ch=s.charAt(i);
-            if(map.containsKey(ch) && map.get(ch)>=rel){
-                rel=map.get(ch)+1;
+        int currLen=0,maxLen=0;
+        int i=0,j=-1;
+        boolean[] set=new boolean[256];
+        while(i<s.length()){
+            char cTA=s.charAt(i);
+            while(set[cTA]==true){
+                j++;
+                char cTR=s.charAt(j);
+                set[cTR]=false;
             }
-            map.put(ch,i);
-            ans=Math.max(ans,i-rel+1);
+            set[cTA]=true;
+            maxLen=Math.max(maxLen,i-j);
+            i++;
         }
-        return ans;
+        return maxLen;
+        
     }
 }
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         int ans=0;
+//         int rel=0;
+//         for(int i=0;i<s.length();i++) {
+//             Character ch=s.charAt(i);
+//             if(map.containsKey(ch) && map.get(ch)>=rel){
+//                 rel=map.get(ch)+1;
+//             }
+//             map.put(ch,i);
+//             ans=Math.max(ans,i-rel+1);
+//         }
+//         return ans;
+//     }
+// }
