ee69185bbf2326ec309e4ce36ae2081f15340d19
Krishna <84918659+krishna101kv@users.noreply.github.com>
2025-01-23 21:49:12 -0800

    Time: 4 ms (99.02%) | Memory: 55.6 MB (37.00%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,67 +1,33 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        //Topo Sort
-        int n=graph.length;
-        List<List<Integer>> adjRev=new ArrayList<>(); 
-        for(int i=0;i<n;i++){
-            adjRev.add(new ArrayList<>());
-        }
-        int indegree[]=new int[n];
-        for(int i=0;i<n;i++){
-            for(int neighbor:graph[i]){
-                adjRev.get(neighbor).add(i);
-                indegree[i]++;
-            }
-        }
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> safeNodes=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0)
-                q.add(i);
-        }
-        while(!q.isEmpty()){
-            int node=q.remove();
-            safeNodes.add(node);
-            for(int it:adjRev.get(node)){
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.add(it);
-            }
-        }
-        Collections.sort(safeNodes);
-        return safeNodes;
-   }
-}
+        int n = graph.length;
+        int[] state = new int[n]; // 0: unvisited, 1: visiting, 2: safe
+        List<Integer> safeNodes = new ArrayList<>();
 
-//         int v=graph.length;
-//         int vis[]=new int[v];
-//         int pathVis[]=new int[v];
-//         int check[]=new int[v];
-//         for(int i=0;i<v;i++){
-//             if(vis[i]==0)
-//                 dfs(i,graph,vis,pathVis,check);
-//         }
-//         List<Integer> safeNodes=new ArrayList<>();
-//         for(int i=0;i<v;i++){
-//             if(check[i]==1)
-//                 safeNodes.add(i);
-//         }
-//         return safeNodes;
-//     }
-//     private boolean dfs(int node,int[][] graph,
-//    int[] vis,int[] pathVis,int[] check){
-//         vis[node]=1;
-//         pathVis[node]=1;
-//         check[node]=0;
-//         for(int it:graph[node]){
-//             if(vis[it]==0){
-//                 if(dfs(it,graph,vis,pathVis,check)==true)
-//                     return true;
-//             }
-//             else if(pathVis[it]==1){
-//                 return true;
-//             }
-//         }//If check[node]=1, the node is safe (not part of a cycle and does not lead to any cycle
-//         check[node]=1;
-//         pathVis[node]=0;
-//         return false;
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, graph, state)) {
+                safeNodes.add(i);
+            }
+        }
+
+        return safeNodes;
+    }
+
+    private boolean dfs(int node, int[][] graph, int[] state) {
+        if (state[node] == 1) return false; // Cycle detected
+        if (state[node] == 2) return true;  // Already safe
+
+        state[node] = 1; // Mark as visiting
+
+        for (int neighbor : graph[node]) {
+            if (!dfs(neighbor, graph, state)) {
+                return false; // If any neighbor is not safe, this node is not safe
+            }
+        }
+
+        state[node] = 2; // Mark as safe
+        return true;
+    }
+}
