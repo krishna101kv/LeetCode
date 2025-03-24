9baa184ff0b1ece72819ba38a1379cf12b75f69a
Krishna <84918659+krishna101kv@users.noreply.github.com>
2025-03-24 07:07:27 -0700

    Time: 0 ms (100.00%) | Memory: 45.3 MB (55.54%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int xor1=0,xor2=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            xor1=nums[i]^xor1;
-            xor2=i^xor2;
+        int res=0;
+        int len=(nums.length*(nums.length+1))/2;
+        for(int i=0;i<nums.length;i++){
+            res+=nums[i];
         }
-        xor2=xor2^n;
-        return xor2^xor1;
+        return len-res;
     }
 }
 // Can be done using sum of all numbers that is n*n+1 and then substracte it to find the missing //numbers
