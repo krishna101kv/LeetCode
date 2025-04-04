32850e190857d58ef538405867a46c4c14cee563
Krishna <84918659+krishna101kv@users.noreply.github.com>
2025-04-04 07:57:00 -0700

    Time: 8 ms (84.72%) | Memory: 45.5 MB (24.62%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,25 +1,54 @@
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int low = 1, high = Integer.MIN_VALUE;
+//         for (int pile : piles) {
+//             high = Math.max(high, pile);
+//         }
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2; 
+//             int time = totalTime(piles, mid);
+
+//             if (time <= h) {
+//                 high = mid - 1;  // Try smaller speed
+//             } else {
+//                 low = mid + 1;   // Need more speed
+//             }
+//         }
+//         return low;
+//     }
+//     public int totalTime(int[] piles, int k) {
+//         int tt = 0;
+//         for (int pile : piles) {
+//             tt += (pile + k - 1) / k;  // Ceiling of integer division
+//         }
+//         return tt;
+//     }
+// }
 class Solution {
-    public int hourly(int[] arr,int h){
-        int total=0;
-        for(int i=0;i<arr.length;i++){
-            total+=Math.ceil((double)arr[i]/(double)h);
-        }
-        return total;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-        int low=1,high=piles[0];
-        for(int i=0;i<piles.length;i++){
-            high=Math.max(high,piles[i]);
-        }
-        while(low<=high){
-            int mid=(low+high)/2;
-            int total=hourly(piles,mid);
-            if(total<=h){
-                high=mid-1;
-            }else{
-                low=mid+1;
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (canEatAll(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return low;
+        
+        return left;
+    }
+    
+    private boolean canEatAll(int[] piles, int speed, int h) {
+        int time = 0;
+        for (int pile : piles) {
+            time += (pile - 1) / speed + 1; // calculate the time required to eat this pile
+            if (time > h) {
+                return false; // if the total time is greater than h, return false
+            }
+        }
+        return true; // if all piles can be eaten within h hours, return true
     }
 }
