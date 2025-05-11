5d56bb66e4e7973260bc776a9ea1f14f6974fdf1
Krishna <84918659+krishna101kv@users.noreply.github.com>
2025-05-11 13:05:42 -0700

    Time: 1 ms (73.16%) | Memory: 45.6 MB (66.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,24 +1,31 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row=matrix.length;
-        int col=matrix[0].length;
-        boolean check[][]=new boolean[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==0){
-                    for(int a=0;a<col;a++){
-                        check[i][a]=true;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // Create a matrix to mark positions to be set to zero
+        boolean[][] toZero = new boolean[rows][cols];
+
+        // Iterate through the original matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    // Mark row and column to be set to zero
+                    for (int a = 0; a < rows; a++) {
+                        toZero[a][j] = true;
                     }
-                    for(int b=0;b<row;b++){
-                        check[b][j]=true;
+                    for (int b = 0; b < cols; b++) {
+                        toZero[i][b] = true;
                     }
                 }
             }
         }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(check[i][j]==true){
-                    matrix[i][j]=0;
+
+        // Set positions to zero based on the markings
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (toZero[i][j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
