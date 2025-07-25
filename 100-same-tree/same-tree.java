e4063975237946d25364edf37e23746466be37f5
Krishna <84918659+krishna101kv@users.noreply.github.com>
2025-07-24 18:14:22 -0700

    Time: 0 ms (100.00%) | Memory: 41 MB (69.05%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -38,17 +38,27 @@ class Solution {
             if(p==null || q== null || p.val!=q.val){
                 return false;
             }
-
             if(p.left!=null || q.left!=null ){
                 queue.add(new Pair(p.left,q.left));
             }
-
             if(p.right!=null || q.right!=null ){
                 queue.add(new Pair(p.right,q.right));
             }
-
         }
         return true;
 
     }
 }
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p==null && q==null){
+//             return true;
+//         }
+//         if(q==null || p==null || p.val!=q.val){
+//             return false;
+//         }
+//         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        
+//     }
+// }
+
